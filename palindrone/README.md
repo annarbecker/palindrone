301e85d31685212501a20cf6a607a26c46a41c78
anna becker and michael smith <michaelanthonysmith00@gmail.com>
2016-01-20 16:09:42 -0800

    Setup files and business logic.

--- palindrone/README.md
+++ palindrone/README.md
@@ -1,4 +1,4 @@
-#This project is called Pig-Latin
+#This project is called Palindrone
 ##By Anna Becker and Michael Smith
 
 ###This project uses
@@ -9,6 +9,6 @@
  * jQuery
 
 ###Description
-This project is a Pig Latin translator.
+This project identifies whether or not a word is a palindrone, giving back a true or false value. 
 
-&copy; 2016 Anna Becker and Michael Smith. All rights reserved. 
+&copy; 2016 Anna Becker and Michael Smith. All rights reserved.
--- palindrone/js/scripts.js
+++ palindrone/js/scripts.js
@@ -1,41 +1,11 @@
-var pigLatin = function(englishWord) {
-  var splitAt = indexOfFirstVowel(englishWord);
-  return englishWord.slice(splitAt) + englishWord.slice(0, splitAt) + "ay";
-};
+var palindrone = function(word) {
+  var normal = word.split("").reverse().join("");
 
-var indexOfFirstVowel = function(word) {
- var vowels = ['a', 'e', 'i', 'o', 'u'];
- var letters = word.split("");
-
- for (var i = 0; i < letters.length; i++) {
-  for (var j = 0; j < vowels.length; j++) {
-    if (letters[i] === vowels[j]) {
-       return i;
-      }
-    }
-  }
-};
-
-var quWords = function(englishWord) {
-  var squ = 'squ';
-  var qu = 'qu';
-  if (englishWord[0] + englishWord[1] + englishWord[2] === squ) {
-    return englishWord.slice(3) + englishWord[0] + englishWord[1] + englishWord[2] + "ay";
+  if (normal === word) {
+    return true;
   } else {
-    return englishWord.slice(2) + englishWord[0] + englishWord[1] + "ay"
+    return false;
   }
-};
 
-$(function(){
- $('form#latin').submit(function(event){
-   var englishWord = $("input#word").val();
-   var translatedMessage = pigLatin(englishWord);
 
-   $(".translation").text(translatedMessage);
-
-   $('#result').show();
-
-   event.preventDefault();
-  });
-
-});
+}
